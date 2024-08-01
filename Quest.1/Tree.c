9197a9922952ce625935c9518caf097e8a879332
Igor Yanko <igoryanko@hotmail.com>
2024-08-01 16:44:43 -0300

    Arquivos dos exercícios dados em sala

---
+++ Quest.1/Tree.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(){
+    printf("\t *\n\t***\n       *****\n       / | \\");
+}
---
+++ Quest.2/Percent.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(){
+    printf("\n TOTAL\t =\t100%%\n IVA\t =\t17%%\n IRS\t =\t15%%\n- - - - - - - - - - -\n LIQ\t =\t68%%");
+}
