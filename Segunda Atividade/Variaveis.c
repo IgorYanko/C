8184697e222a2837a5e3bdd8d87358d4dbf086ed
Theveribas <igoryanko@hotmail.com>
2024-08-11 23:04:23 -0300

    Added the second activity

--- Segunda Atividade/Variaveis.c
+++ Segunda Atividade/Variaveis.c
@@ -2,4 +2,9 @@
 
 int main(){
     
+    int numero1 = 20, numero2 = 5, soma = 25, subtracao = 15, multiplicacao = 100, resto = 0;
+    float divisao = 4;
+
+    printf("Numero 1: %d\nNumero 2: %d\nSoma: %d\nSubtracao: %d\nDivisao: %.2f\nMultiplicacao: %d\nResto: %d\n", 
+           numero1, numero2, soma, subtracao, divisao, multiplicacao, resto);
 }
