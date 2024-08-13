9166a13ecf03c3c1729411cd4f612de57288728b
Theveribas <igoryanko@hotmail.com>
2024-08-13 17:36:00 -0300

    Adicionei switch case pra escolher quais dos exemplos utilizar

--- Segunda Atividade/Variaveis.c
+++ Segunda Atividade/Variaveis.c
@@ -1,10 +1,52 @@
 #include <stdio.h>
 
-int main(){
-    
-    int numero1 = 20, numero2 = 5, soma = 25, subtracao = 15, multiplicacao = 100, resto = 0;
-    float divisao = 4;
+int main() {
+    int numero1, numero2, soma, subtracao, multiplicacao, resto;
+    float divisao;
+    int escolha;
 
-    printf("Numero 1: %d\nNumero 2: %d\nSoma: %d\nSubtracao: %d\nDivisao: %.2f\nMultiplicacao: %d\nResto: %d\n", 
-           numero1, numero2, soma, subtracao, divisao, multiplicacao, resto);
+    printf("Escolha o par de numeros que deseja utilizar:\n");
+    printf("1. -20 e 5\n");
+    printf("2. -30 e 4\n");
+    printf("3. -40 e 3\n");
+    printf("4. -50 e 2\n");
+    printf("5. -60 e 1\n");
+    printf("Escolha uma opcao: ");
+    scanf("%d", &escolha);
+
+    switch(escolha) {
+        case 1:
+            numero1 = -20;
+            numero2 = 5;
+            break;
+        case 2:
+            numero1 = -30;
+            numero2 = 4;
+            break;
+        case 3:
+            numero1 = -40;
+            numero2 = 3;
+            break;
+        case 4:
+            numero1 = -50;
+            numero2 = 2;
+            break;
+        case 5:
+            numero1 = -60;
+            numero2 = 1;
+            break;
+        default:
+            printf("Opcao invalida.\n");
+            return 1; 
+    }
+
+    soma = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = (float) numero1 / numero2;
+    resto = numero1 % numero2;
+
+    printf("Numero 1: %d\nNumero 2: %d\n", numero1, numero2);
+    printf("Soma: %d\nSubtracao: %d\nMultiplicacao: %d\nDivisao: %.2f\nResto: %d\n", 
+           soma, subtracao, multiplicacao, divisao, resto);
 }
